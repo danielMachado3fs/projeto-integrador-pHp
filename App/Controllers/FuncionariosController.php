356c7925a964b02869dfc49f607968f16fb57ec0
Daniel Machado <danielmachado3fs@gmail.com>
2022-11-17 01:14:10 -0300

    feat(funcionarioUpdate): adicionado parcialmente tela para atualizar funcionario

--- App/Controllers/FuncionariosController.php
+++ App/Controllers/FuncionariosController.php
@@ -47,8 +47,36 @@ class FuncionariosController extends Action {
 		->_set('complemento', $_POST['complemento'])
 		->_set('setor', $_POST['setor']);
 
-		$produtos = $this->funcionarioModel->salvar();
-		var_dump($produtos);
+		$save_id = $this->funcionarioModel->salvar();
+		if($save_id){
+			$this->index();
+		}
+
+	}
+
+	public function update(){
+		$profissional = 
+		$this->render('update');
+	}
+
+	public function update_store(){
+		$funcionarioModel = Container::getModel('Funcionario');
+		$funcionarioModel->_set('nome', $_POST['nome'])
+		->_set('dataNascimento', $_POST['dataNascimento'])
+		->_set('cpf', $_POST['cpf'])
+		->_set('email', $_POST['email'])
+		->_set('telefone', $_POST['telefone'])
+		->_set('cep', $_POST['cep'])
+		->_set('cidade', $_POST['cidade'])
+		->_set('estado', $_POST['estado'])
+		->_set('logradouro', $_POST['logradouro'])
+		->_set('bairro', $_POST['bairro'])
+		->_set('numero', $_POST['numero'])
+		->_set('complemento', $_POST['complemento'])
+		->_set('setor', $_POST['setor']);
+
+		$save_id = $funcionarioModel->salvar();
+		var_dump($save_id);
 	}
 
 }
---
+++ App/Views/funcionarios/edit.php
@@ -0,0 +1,90 @@
+<div class="formularioFuncionarios">
+    <div id="divPrincipalForm">
+        <div class="title">
+            <h1>Adicionar Funcionário</h1>
+        </div>
+        <div class="divForm">
+          <div id="form">
+            <form action="/update_store" method="post">
+              <div class="inputFormName">
+                <div>
+                  <label for="nome">Nome</label>
+                  <input class="input"  class="inputName" type="text" name="nome" id="nome" placeholder="Ex: Joao Silva"
+                    required>
+                </div>
+              </div>
+              <div class="divFormulario">
+                <div class="contentsFormulario">
+                  <div>
+                    <div class="inputForm">
+                      <label for="dataNascimento">Data de nascimento</label>
+                      <input class="input" type="date" name="dataNascimento" id="dataNascimento" required>
+                    </div>
+                    <div class="inputForm">
+                      <label for="email">Email</label>
+                      <input class="input" type="email" name="email" id="email" placeholder="joao@exemplo.com" required>
+                    </div>
+                    <div class="inputForm">
+                      <label for="cep">CEP</label>
+                      <input class="input buscaCep" type="text" name="cep" id="cep" placeholder="0000-000" required>
+                    </div>
+                    <div class="inputForm">
+                      <label for="estado">Estado</label>
+                      <input class="input" type="text" name="estado" id="estado" readonly required>
+                    </div>
+                    <div class="inputForm">
+                      <label for="bairro">Bairro</label>
+                      <input class="input" type="text" name="bairro" id="bairro" readonly required>
+                    </div>
+                    <div class="inputForm">
+                      <label for="numero">Numero</label>
+                      <input class="input" type="number" name="numero" id="numero" required>
+                    </div>
+                    <div class="inputForm">
+                      <label for="setor">Setor</label>
+                      <input class="input" type="text" name="setor" id="setor" required>
+                    </div>
+                  </div>
+                </div>
+                <div class="contentsFormulario2">
+                  <div class="inputForm">
+                    <label for="cpf">CPF</label>
+                    <input class="input" type="text" name="cpf" id="cpf" required placeholder="000.000.000-00" required>
+                  </div>
+                  <div class="inputForm">
+                    <label for="telefone">Telefone</label>
+                    <input class="input" type="text" name="telefone" id="telefone" required
+                      placeholder="(00) 90000-0000">
+                  </div>
+                  <div class="inputForm">
+                    <label for="cidade">Cidade</label>
+                    <input class="input" type="text" name="cidade" id="cidade" readonly required>
+                  </div>
+                  <div class="inputForm">
+                    <label for="logradouro">Logradouro</label>
+                    <input class="input" type="text" name="logradouro" id="logradouro" readonly required>
+                  </div>
+                  <div class="inputForm">
+                    <label for="complemento">Complemento</label>
+                    <input class="input" type="text" name="complemento" id="complemento"
+                      placeholder="Casa, apartamento, condomínio" required>
+                  </div>
+                </div>
+                </div>
+                <div class="rodape_btn">
+                    <button  id="sendBtn" type="submit">Salvar</button>
+                    <button id="cancelBtn">Cancelar</button>
+                </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script>
+        $(document).ready(function(){
+            $("#cep").mask("00000-000");
+            $("#cpf").mask("000.000.000-00");
+            $("#telefone").mask("(00) 00000-0000");
+        })
+    </script>
