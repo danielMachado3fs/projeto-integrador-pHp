7db6b2d2433d3e6d65f49258bb31669c5a0d30ae
fsanntiago <zerofabricio@hotmail.com>
2022-11-23 03:43:43 -0300

    atualizando tabela veiculo.sql

--- public/database/veiculo.sql
+++ public/database/veiculo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Tempo de geração: 18-Nov-2022 às 05:06
--- Versão do servidor: 10.4.24-MariaDB
--- versão do PHP: 7.4.29
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 23-Nov-2022 às 06:38
+-- Versão do servidor: 8.0.27
+-- versão do PHP: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,8 +27,9 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `veiculo`
 --
 
-CREATE TABLE `veiculo` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `veiculo`;
+CREATE TABLE IF NOT EXISTS `veiculo` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `placa` varchar(7) NOT NULL,
   `modelo` varchar(60) NOT NULL,
   `marca` varchar(60) NOT NULL,
@@ -36,35 +37,12 @@ CREATE TABLE `veiculo` (
   `tipo` varchar(60) NOT NULL,
   `dataAquisicao` date NOT NULL,
   `valor` decimal(10,2) NOT NULL,
-  `deleted` tinyint(4) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `veiculo`
---
-
-INSERT INTO `veiculo` (`id`, `placa`, `modelo`, `marca`, `anoFabricacao`, `tipo`, `dataAquisicao`, `valor`, `deleted`) VALUES
-(1, 'ABC1234', 'S10', 'CHEVROLET', '2022-11-15', 'carro', '2022-11-15', '50000.00', 0);
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `veiculo`
---
-ALTER TABLE `veiculo`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `veiculo`
---
-ALTER TABLE `veiculo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  `deletado` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UQ_PLACA` (`placa`),
+  KEY `IX_MARCA` (`marca`),
+  KEY `IX_MARCA_TIPO` (`marca`,`tipo`)
+) ENGINE=InnoDB AUTO_INCREMENT=151 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
