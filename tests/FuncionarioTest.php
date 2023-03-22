cc4c80ef02dcae214f4e12a7daad50693809e26a
DanielMachado <danielmachado3fs@gmail.com>
2023-03-22 20:29:13 -0300

    feat: adiciona xml de configuração php unit

--- tests/FuncionarioTest.php
+++ tests/FuncionarioTest.php
@@ -10,4 +10,14 @@ class FuncionarioTest extends TestCase
         $profissional = Container::getModel('Funcionario');
         $this->assertNotEmpty($profissional->_get("values"));
     }
+
+    public function test_set_values(){
+        $profissional = Container::getModel('Funcionario');
+        //seta um valor no atributo cpf
+        $profissional->_set('cpf', 123456789);
+        //pega o valor do atributo cpf
+        $atributo = $profissional->_get('cpf');
+        
+        $this->assertEquals(123456789, $atributo);
+    }
 }
